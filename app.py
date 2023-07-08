db63233f5c4dcb3052bce032a790f37fe0fd8960
dineshdyne <dineshkumar11235@gmail.com>
2023-07-08 21:02:25 +0530

    Repayment page - UI Changes

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import numpy as np
 import math
 from itertools import *
 import streamlit.components.v1 as stc
+from funcs import *
+import locale
+locale.setlocale(locale.LC_ALL, 'en_IN.utf8')
+
 #from more_itertools import *
 
 import plotly.express as px
@@ -17,30 +21,6 @@ st.set_page_config(  # Alternate names: setup_page, page, layout
 )
 
 
-def sip(investment, tenure, interest, amount=0, is_year=True, is_percent=True, inflation_rate=0, rate_of_increase=0, stop_year=None):
-    tenure = tenure*12 if is_year else tenure
-    interest = interest/100 if is_percent else interest
-    interest /= 12
-    amount_every_month = []
-    invested = amount
-    inf_invested = amount
-    for month in range(tenure):
-        nth_year = month % 12
-        if nth_year == 0:
-            investment = investment*(100+rate_of_increase)/100
-        if stop_year*12 <= month:
-            investment = 0
-        invested = invested+investment
-        inf_invested = inf_invested+investment / \
-            (1+inflation_rate/1200)**(month)
-        amount = (amount + investment)*(1+interest)
-        amount_every_month.append(
-            [amount, invested, amount-invested, inf_invested])
-    return {'Amount @ Maturity': amount, 'Amount every month': pd.DataFrame(amount_every_month, columns=['total', 'invested', 'profit', 'inflation_adj_invested'])}
-
-
-def emi(p, r, n):
-    return p * r * ((1+r)**n)/((1+r)**n - 1)
 
 
 # #image = Image.open("images/tvs-logo.png")
@@ -48,6 +28,8 @@ def emi(p, r, n):
 st.sidebar.title(f"Investment Calculator")
 
 st.header("Welcome!!!")
+
+
 calc_type = st.sidebar.selectbox(
     'select type', options=['INVESTMENT', 'REPAYMENT'])
 if calc_type == 'INVESTMENT':
@@ -58,7 +40,7 @@ if calc_type == 'INVESTMENT':
     stop_year = col1.number_input(
         "Investment Stop Year", min_value=0, max_value=select_tenure, step=1, value=select_tenure)
     inflation = col2.number_input(
-        "Inflation (yearly) in percentage", min_value=0.0, max_value=14.0, value=5.5, step=0.1)
+        "Inflation (yearly) in percentage", min_value=0.0, max_value=14.0, value=6.0, step=0.1)
 
     monthly_inv = col1.number_input(
         "Monthly inv", min_value=0, value=5000, step=100)
@@ -82,31 +64,41 @@ if calc_type == 'INVESTMENT':
     st.markdown(
         f"""Total : <span style="color:green">{int(final_vals['total'])}</span>""", unsafe_allow_html=True)
     st.write(
-        f"""Equalent to Present day: <span style="color:green">{int(final_vals['total']/((100+inflation)/100)**select_tenure)}</span>""", unsafe_allow_html=True)
+        f"""Equivalent to Present day: <span style="color:green">{int(final_vals['total']/((100+inflation)/100)**select_tenure)}</span>""", unsafe_allow_html=True)
 elif calc_type == "REPAYMENT":
-    select_tenure = st.slider("select tenure in yrs",
-                              min_value=1, max_value=50, value=4, step=1)
-    col1, col2 = st.columns([1, 1])
+    
+    col1, col2 = st.columns([1, 3])
     loan_amt = col1.number_input(
         "Enter Loan Amt", min_value=0, step=1000, value=0)
-    rate_of_interest = col2.slider(
-        "Loan rate of interest", min_value=0.05, value=8.0, step=0.05, max_value=30.0)
-    inflation = col2.number_input(
-        "Inflation (yearly) in percentage", min_value=0.0, max_value=14.0, value=5.5, step=0.1)
+    select_tenure = col2.slider("Loan Term (in years)",
+                              min_value=1, max_value=50, value=20, step=1)
+
+    rate1,rate2=st.columns([1,1])
+    rate_of_interest = rate1.slider(
+        "Loan Interest Rate", min_value=0.05, value=8.0, step=0.05, max_value=30.0)
+    inflation = rate2.number_input(
+        "Inflation Rate", min_value=0.0, max_value=14.0, value=6.0, step=0.1)
 
     emi_amt = emi(loan_amt, rate_of_interest/1200, select_tenure*12)
-    st.markdown(
-        f"""EMI : <span style="color:green">{emi_amt}</span>""", unsafe_allow_html=True)
 
-    col1, col2 = st.columns([1, 1])
-    extra_payment = col1.number_input(
-        "Enter extra payment", min_value=0, step=100)
+    col1,col2,col3=st.columns([1,3.6,1])
+    col_x,col_y=col2.columns([1,1])
+    #col1,col2,col3,col4=st.columns([1,1.8,1.8,1],gap="medium")
 
-    rate_of_increment = col2.slider(
+    col_x.metric("EMI",locale.format_string('%d', int(emi_amt), grouping=True),f"yearly: {locale.format_string('%d', int(emi_amt*12), grouping=True)}","inverse")
+
+    col_y.metric("Interest Rate",rate_of_interest ,f"{rate_of_interest-inflation:.2f} from Inflation","inverse")
+    # st.markdown(
+    #     f"""EMI : <span style="color:green">{emi_amt}</span>""", unsafe_allow_html=True)
+
+    extra_payment = col_x.number_input(
+        "Extra Monthly Repayment", min_value=0, step=100)
+    rate_of_increment = col_y.slider(
         "Rate of increment", min_value=-20.0, step=0.5, value=0.0, max_value=20.0)
-    emi_rate_of_increment = col2.slider(
-        "EMI Rate of increment", min_value=0.0, step=0.1, value=0.0, max_value=20.0)
 
+    emi_rate_of_increment = col2.slider(
+        ">Increase EMI (yearly rate)", min_value=0.0, step=0.1, value=0.0, max_value=20.0)
+    
     principle = []
     interest = []
     extra = []
@@ -114,8 +106,14 @@ elif calc_type == "REPAYMENT":
     num_months = 0
     inf_invested = 0
     emi_extra = emi_amt
+    # import time
+    # my_bar = st.progress(0, text="Running calculations")
     while loan_amt > 0:
+        
+        
         num_months += 1
+        # time.sleep(.01)
+        # my_bar.progress(num_months,text="Running calculations")
         int_comp = loan_amt*rate_of_interest/1200
         princ_comp = emi_amt-int_comp
         if num_months % 12 == 0:
@@ -131,17 +129,29 @@ elif calc_type == "REPAYMENT":
         interest.append(int_comp)
         principle.append(princ_comp)
     # st.write(int(loan_amt))
-    st.markdown(
-        f"""Number of Months: <span style="color: green">{num_months}</span>""", unsafe_allow_html=True)
-
     df = pd.DataFrame(zip(principle,  extra, interest, emi_inc),
-                      columns=['principle', 'extra', 'interest', 'emi_inc'])
+                      columns=['Principle', 'Extra_Repayment', 'Interest', 'EMI_increment'])
     df = df.loc[:, (df != 0).any(axis=0)]
-    st.markdown(
-        f"""Total Paid :  <span style="color:green">{int(df.sum().sum())}</span>""", unsafe_allow_html=True)
-    st.markdown(
-        f"""Total Paid : Inflation Adjusted <span style="color:green">{int(inf_invested)}</span>""", unsafe_allow_html=True)
+
+
+    col0,col1,col2,col3=st.columns([0.5,1.5,1.5,1.5])
+
+    col1.metric("Months of Repayment",str(num_months),f"years saved:{select_tenure-num_months/12:.2f}")
+    savings=locale.format_string('%d', int(emi_amt*select_tenure*12-df.sum().sum()), grouping=True)
+    col2.metric("Total Repayment.",locale.format_string("%d", int(df.sum().sum()), grouping=True),f"savings: {savings}")
+    col3.metric("Total repayment(inflation Adjusted)",locale.format_string("%d", int(inf_invested), grouping=True))
+    # st.markdown(
+    #     f"""Number of Months: <span style="color: green">{num_months}</span>""", unsafe_allow_html=True)
+
+    # st.markdown(
+    #     f"""Total Paid :  <span style="color:green">{int(df.sum().sum())}</span>""", unsafe_allow_html=True)
+    # st.markdown(
+    #     f"""Total Paid : Inflation Adjusted <span style="color:green">{int(inf_invested)}</span>""", unsafe_allow_html=True)
+
+    st.markdown("<h2 style='text-align: center; '>Chart Representation</h2>", unsafe_allow_html=True)
     st.plotly_chart(
-        px.bar(df, y=list(df.columns),
+        px.bar(df, y=list(df.columns),labels={"index":'Months',"value":"Monthly Payments"}
                #color_discrete_sequence=['green', 'blue', 'red', 'cyan']
                ), use_container_width=True)
+    
+
---
+++ funcs.py
@@ -0,0 +1,30 @@
+
+import pandas as pd
+
+def sip(investment, tenure, interest, amount=0, is_year=True, is_percent=True, inflation_rate=0, rate_of_increase=0, stop_year=None):
+    tenure = tenure*12 if is_year else tenure
+    interest = interest/100 if is_percent else interest
+    interest /= 12
+    amount_every_month = []
+    invested = amount
+    inf_invested = amount
+    for month in range(tenure):
+        nth_year = month % 12
+        if nth_year == 0:
+            investment = investment*(100+rate_of_increase)/100
+        if stop_year*12 <= month:
+            investment = 0
+        invested = invested+investment
+        inf_invested = inf_invested+investment / \
+            (1+inflation_rate/1200)**(month)
+        amount = (amount + investment)*(1+interest)
+        amount_every_month.append(
+            [amount, invested, amount-invested, inf_invested])
+    return {'Amount @ Maturity': amount,
+            'Amount every month': pd.DataFrame(amount_every_month, columns=['total', 'invested', 'profit', 'inflation_adj_invested'])}
+
+def emi(p, r, n):
+    return p * r * ((1+r)**n)/((1+r)**n - 1)
+
+
+
